5e0f9bfd716056718e5e839daa82ffbdc763bdb3
Tuhaj <zrazic@wp.pl>
2017-07-03 23:11:57 +0200

    adds and tries to use react router dom

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Welcome extends React.Component {
+  render() { return <div>Say hello to my little friend!</div> }
+}
+
+class Goodbye extends React.Component {
+  render() { return <div>Farewell!</div> }
+}
+
+
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
+    <BrowserRouter>
+      <div>
+        <Route path="/hello" component={Welcome}/>
+        <Route path="/goodbye" component={Goodbye}/>
+      </div>
+    </BrowserRouter>
   </Provider>
   , document.querySelector('.container'));
